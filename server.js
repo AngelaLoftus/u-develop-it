5b845542261263269c0e12a7753fa5fcd5bdb3c6
Angela Loftus <angelaloftus@yahoo.com>
2021-10-20 13:42:48 -0500

    connect to election database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username,
+        user: 'root',
+        //your MYSql pasword
+        password: '<3MonkeyIsland',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 //default response for any other request (NOT FOUND)
